5c0430e13b5dfb7e4218b831af89db1f2cf4f0d9
IBTechRaj <raj_shk@rediffmail.com>
2021-04-30 14:37:36 +0530

    duplicate test removed

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Link, type: :model do
 
   it "is invalid if it does not have a lookup code" do
     link=Link.new(
-        original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+        original_url: "https://link.springer.com/chapter/10.1057%2F9781403980212_4",
         
     )
     expect(link.valid?).to be(false)
@@ -15,6 +15,24 @@ it "is invalid if it does not have an original URL" do
         lookup_code: "1234567"
     )
     expect(link.valid?).to be(false)
+end
+
+
+it "is invalid if the lookup code already exists" do
+    link=Link.new(
+        original_url: "https://link.springer.com/chapter/10.1057%2F9781403980212_4",
+        lookup_code: "1234567"
+    )
+    link.save
+
+    link_2=Link.new(
+        original_url: "https://link.springer.com/chapter/10.1057%2F9781403980212_4",
+        lookup_code: "1234567"
+    )
+    link.save
+
+
+    expect(link_2.valid?).to be(false)
 end
 
 # it "is valid if it has an original URL and a lookup code" do
