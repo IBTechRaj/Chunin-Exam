705edbc8e35634c0136e7f0557d0947969b025fd
IBTechRaj <raj_shk@rediffmail.com>
2021-04-30 11:50:42 +0530

    unique url shortener test passed

--- spec/services/shortener_spec.rb
+++ spec/services/shortener_spec.rb
@@ -7,5 +7,17 @@ shortener = Shortener.new(url)
 expect(shortener.lookup_code.length).to eq(7)
 end
 
+it "gives each URL its own lookup code" do
+
+    url="https://link.springer.com/chapter/10.1057%2F9781403980212_4"
+    shortener = Shortener.new(url)
+    code_1 = shortener.lookup_code
+
+    url="https://link.springer.com/chapter/10.1057%2F9781403980214_8"
+    shortener = Shortener.new(url)
+    code_2 = shortener.lookup_code
+
+    expect(code_2).not_to eq(code_1)
+end
 
 end
