d67453e5e275c57ad6a9e7c3b02cba24a3df24ed
IBTechRaj <raj_shk@rediffmail.com>
2021-04-30 14:32:39 +0530

    url without code and code without url tests passed

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-# RSpec.describe Link, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# # end
+
+RSpec.describe Link, type: :model do
+
+  it "is invalid if it does not have a lookup code" do
+    link=Link.new(
+        original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+        
+    )
+    expect(link.valid?).to be(false)
+end
+it "is invalid if it does not have an original URL" do
+    link=Link.new(
+        lookup_code: "1234567"
+    )
+    expect(link.valid?).to be(false)
+end
+
+# it "is valid if it has an original URL and a lookup code" do
+#     link=Link.new(
+#         original_url: "therhejrhe",
+#         lookup_code: "1234567"
+#     )
+#     expect(link.valid?).to be(false)
+# end
+end
