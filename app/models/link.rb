242df403f68223cc87a4027ad78a56da47d2659a
IBTechRaj <raj_shk@rediffmail.com>
2021-04-30 17:08:46 +0530

    redirection of shortened link to original link test passed

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,9 @@ class Link < ApplicationRecord
     validates_presence_of :lookup_code, :original_url
     validates_uniqueness_of :lookup_code
     # validate :original_url_format
+
+
+    def shortened_url
+"http://localhost:3000/#{lookup_code}"
+    end
 end
