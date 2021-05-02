f63b91b797b904b79eb5f0c3248ddcf3bc579990
IBTechRaj <raj_shk@rediffmail.com>
2021-05-02 20:07:31 +0530

    a few stats included

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
     @link = Link.new
+
+    # render plain: "You IP address is #{client_ip}"
   end
+
+  private def client_ip
+    request.ip
+  end
+
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,18 +2,23 @@ class LinksController < ApplicationController
 
     def show
         link = Link.find_by(lookup_code: params[:lookup_code])
-    redirect_to link.original_url
+        redirect_to link.original_url
     end
     
     def create
         shortener = Shortener.new(link_params[:original_url])
         @link = shortener.generate_short_link
+         @url_count = Link.count
         
         if @link.persisted?
             respond_to :js 
         else
             render 'err.js.erb'
         end
+    end
+
+    def show_data
+        @url_count = Link.count
     end
 
     private
